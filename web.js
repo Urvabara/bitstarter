b6728bf3d3b4207eeaa88328219f69ee6e1c27a8
Henri Heinonen <henri.heinonen@gmail.com>
2013-07-09 15:43:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,19 +1,16 @@
-#!/usr/bin/env node
 var express = require('express');
-
+ 
 var app = express.createServer(express.logger());
-
+ 
 var fs = require('fs');
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  var buffer = new Buffer(32000);
-  buffer.write(data);
-});
-
+var data = fs.readFileSync('index.html','utf8');
+var buffer = new Buffer();
+buffer.write(data, "utf-8");
+ 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString('utf-8'));
 });
-
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
