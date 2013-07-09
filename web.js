0a9a3582179fa88fabdfc61183a21174b7bce99d
Henri Heinonen <henri.heinonen@gmail.com>
2013-07-09 15:38:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var data = fs.readFileSync('index.html','utf8');
-var buffer = new Buffer(32000);
-buffer.write(data);
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  var buffer = new Buffer(32000);
+  buffer.write(data);
+});
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
