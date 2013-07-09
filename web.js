4358824998999db9565fae7b96ad99884f7833d1
Henri Heinonen <henri.heinonen@gmail.com>
2013-07-09 14:19:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var data = fs.readFileSync('index.html','utf8');
+var buffer = new Buffer(16);
+buffer.write(data, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
