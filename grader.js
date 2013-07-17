fdd4f5dc7aeb615a8697f83fe45bf063810883a6
Henri Heinonen <henri.heinonen@gmail.com>
2013-07-17 12:10:46 +0000

    A new version of grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_DEFAULT = "html.txt";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://fierce-tundra-7865.herokuapp.com/";
+var sys = require('util');
+var rest = require('../node_modules/restler');
+var outfile = HTMLFILE_DEFAULT;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +69,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Enter the URL')
         .parse(process.argv);
+    rest.get(program.url).on('complete', function(result) {
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+      } else {
+        sys.puts(result);
+        fs.writeFileSync(outfile, result);
+      }
+    });
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
